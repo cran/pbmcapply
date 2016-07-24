ef2c098dacc294fb3c28da2b8b844b153ea52252
Kevin kuang <kvn.kuang@mail.utoronto.ca>
2016-07-24 17:10:47 +0000

    version 1.1.0

--- R/pbmclapply.R
+++ R/pbmclapply.R
@@ -1,52 +1,48 @@
-library(parallel)
+PORT = 6311
 
-pbmclapply <- function(X,
-                       FUN,
-                       ...,
-                       mc.preschedule = TRUE,
-                       mc.set.seed = TRUE,
-                       mc.silent = FALSE,
-                       mc.cores = getOption("mc.cores", 2L),
-                       mc.cleanup = TRUE,
-                       mc.allow.recursive = TRUE,
-                       mc.progress = TRUE,
-                       mc.style = 3)
-{
-  if (!is.vector(X) || is.object(X))
+pbmclapply <- function(X, FUN, ..., mc.style = 3, mc.cores =getOption("mc.cores", 2L)) {
+
+  plan(multiprocess)
+
+  if (!is.vector(X) || is.object(X)) {
     X <- as.list(X)
-
-  if (mc.progress) {
-    # Create a separate worker process to monitor the progress of mclapply
-    cl <- makeCluster(1, useXDR = T)
-    clusterCall(cl, function(length) {
-      .GlobalEnv$progress <- 0
-      text <- capture.output(.GlobalEnv$pb <- txtProgressBar(0, length, style = mc.style))
-      cat(text, file = 1)
-      setTxtProgressBar(.GlobalEnv$pb, 0)
-    }, length(X))
   }
 
-  tryCatch({
-    result <- mclapply(X, function(...) {
+  progressMonitor <- futureCall(function(X, FUN, mc.cores, ...) {
+    socketServer <- socketConnection(open = "wb", port = PORT, blocking = T, server = T)
+    tryCatch(result <- mclapply(X, function(...) {
       res <- FUN(...)
-      if (mc.progress) {
-        # Inform the monitor worker process about the completion
-        clusterCall(cl, function() {
-          .GlobalEnv$progress <- .GlobalEnv$progress + 1
-          text <- capture.output(setTxtProgressBar(.GlobalEnv$pb, .GlobalEnv$progress))
-          cat(text, file = 1)
-        })
-      }
+      writeBin(1, socketServer)
       return(res)
-    }, ...,
-    mc.preschedule = mc.preschedule, mc.set.seed = mc.set.seed,
-    mc.silent = mc.silent, mc.cores = mc.cores,
-    mc.cleanup = mc.cleanup, mc.allow.recursive = mc.allow.recursive)
-  }, finally = {
-    if (mc.progress) {
-      stopCluster(cl)
-    }
-  })
+    }, ..., mc.cores = mc.cores),
+    finally = {
+      close(socketServer)
+    })
 
-  return(result)
+    return(result)
+  }, args = list(X, FUN, ..., mc.cores))
+
+  length <- length(X)
+  pb <- txtProgressBar(0, length, style = mc.style)
+  setTxtProgressBar(pb, 0)
+  progress <- 0
+
+  # Create a socket client and update progress bar accordingly
+  isCreated <- F
+  while(!isCreated) {
+    Sys.sleep(0.5)
+    try(socketClient <- socketConnection(open = "rb", port = PORT, blocking = T, server = F), silent = T)
+    if (exists("socketClient")) {
+      isCreated <- T
+      while (progress < length) {
+        readBin(socketClient, "double")
+        progress <- progress + 1
+        setTxtProgressBar(pb, progress)
+      }
+      close(socketClient)
+    }
+  }
+
+  # Retrieve the result from the future
+  return(value(progressMonitor))
 }
--- R/pbmcmapply.R
+++ R/pbmcmapply.R
@@ -1,64 +1,50 @@
-library(parallel)
+PORT = 6311
 
-pbmcmapply <- function(FUN,
-                       ...,
-                       MoreArgs = NULL,
-                       mc.preschedule = TRUE,
-                       mc.set.seed = TRUE,
-                       mc.silent = FALSE,
-                       mc.cores = getOption("mc.cores", 2L),
-                       mc.cleanup = TRUE,
-                       mc.progress = TRUE,
-                       mc.style = 3)
-{
-  if (mc.progress) {
-    # Get the number of worker process
-    length <- max(mapply(function(element) {
-      if (is.null(nrow(element))) {
-        return(length(element))
-      } else {
-        return(nrow(element))
+pbmcmapply <- function(FUN, ..., MoreArgs = NULL, mc.style = 3, mc.cores =getOption("mc.cores", 2L)) {
+
+  plan(multiprocess)
+
+  progressMonitor <- futureCall(function(FUN, MoreArgs, mc.cores, ...) {
+    socketServer <- socketConnection(open = "wb", port = PORT, blocking = T, server = T)
+    tryCatch(result <- mcmapply(function(...) {
+      res <- FUN(...)
+      writeBin(1, socketServer)
+      return(res)
+    }, ..., MoreArgs = MoreArgs, mc.cores = mc.cores),
+    finally = {
+      close(socketServer)
+    })
+
+    return(result)
+  }, args = list(FUN, MoreArgs, mc.cores, ...))
+
+  length <- max(mapply(function(element) {
+    if (is.null(nrow(element))) {
+      return(length(element))
+    } else {
+      return(nrow(element))
+    }
+  }, list(...)))
+  pb <- txtProgressBar(0, length, style = mc.style)
+  setTxtProgressBar(pb, 0)
+  progress <- 0
+
+  # Create a socket client and update progress bar accordingly
+  isCreated <- F
+  while(!isCreated) {
+    Sys.sleep(0.5)
+    try(socketClient <- socketConnection(open = "rb", port = PORT, blocking = T, server = F), silent = T)
+    if (exists("socketClient")) {
+      isCreated <- T
+      while (progress < length) {
+        readBin(socketClient, "double")
+        progress <- progress + 1
+        setTxtProgressBar(pb, progress)
       }
-    }, list(...)))
-
-    # Create a separate worker process to monitor the progress of mcmapply
-    cl <- makeCluster(1, useXDR = T)
-    clusterCall(cl, function(length) {
-      .GlobalEnv$progress <- 0
-      text <- capture.output(.GlobalEnv$pb <- txtProgressBar(0, length, style = mc.style))
-      cat(text, file = 1)
-      setTxtProgressBar(.GlobalEnv$pb, 0)
-      return(0)
-    }, length)
+      close(socketClient)
+    }
   }
 
-  tryCatch({
-    result <- mcmapply(
-      function(...) {
-        res <- FUN(...)
-        if (mc.progress) {
-          clusterCall(cl, function() {
-            .GlobalEnv$progress <- .GlobalEnv$progress + 1
-            text <- capture.output(setTxtProgressBar(.GlobalEnv$pb, .GlobalEnv$progress))
-            cat(text, file = 1)
-            return(0)
-          })
-        }
-        return(res)
-      },
-      ...,
-      MoreArgs = MoreArgs,
-      mc.preschedule = mc.preschedule,
-      mc.set.seed = mc.set.seed,
-      mc.silent = mc.silent,
-      mc.cores = mc.cores,
-      mc.cleanup = mc.cleanup
-    )
-  }, finally = {
-    if (mc.progress) {
-      stopCluster(cl)
-    }
-  })
-
-  return(result)
+  # Retrieve the result from the future
+  return(value(progressMonitor))
 }
